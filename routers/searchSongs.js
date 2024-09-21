3358ac4a788c798c4e73106d317b91cf4924f3f8
Arjun Gade <139616230+gadearjun24@users.noreply.github.com>
2024-09-21 08:45:30 +0530

    Update searchSongs.js

--- routers/searchSongs.js
+++ routers/searchSongs.js
@@ -4,7 +4,7 @@ const SearchSongController = require("../controllers/searchSongs");
 const router = express.Router();
 
 router
-  .post("/searchsongs/:name", SearchSongController.insertSearchSongs)
+  .get("/searchsongs/:name", SearchSongController.insertSearchSongs)
   .get("/searchsongs/", SearchSongController.getAllSearchSongs);
 
 exports.router = router;
